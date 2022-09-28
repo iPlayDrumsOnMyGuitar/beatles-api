ecc703ebc25783d00a6a7c24ebbf120b8fd5e703
Brian Payne <payne.in.the.brian@gmail.com>
2022-09-28 18:35:54 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+const beatle = {
+        'john lennon':{
+            'vocals': true,
+            'origin': 'Liverpool',
+            'Born': 1940,
+            'Death': 1980,
+            'instruments': 'Guitar and Piano/Keys'
+        },
+        'paul mccartney':{
+            'vocals': true,
+            'origin': 'Liverpool',
+            'Born': 1942,
+            'Death': 'TBD',
+            'instruments': 'Bass, Piano/Keys, Guitar, and Drums'
+        },
+        'george harrison':{
+            'vocals': true,
+            'origin': 'Liverpool',
+            'Born': 1943,
+            'Died': 2001,
+            'instruments': "Guitar and Sitar"
+        },
+        'ringo starr':{
+            'vocals': true,
+            'origin': 'Liverpool',
+            'Born': 1940,
+            'Died': 'TBD',
+            'instruments': 'Drums and Percussion'
+        },
+        'honorary-beatle':{
+            'beatle': 'honorary beatle'
+            'vocals': 'tries',
+            'origin': 'unknown',
+            'Born': 'unknown',
+            'Died': 'unknown',
+            'Instruments': 'tamborine',
+            
+        },
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+    const beatleName = request.params.name.toLowerCase()
+    if( beatle[beatleName] ){
+        response.json(beatle[beatleName])
+    }else{
+        response.json(beatle['honorary-beatle'])
+    }
+    response.json(beatle)
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is running on port ${PORT}! Betta Go Catch It!`)
+})
